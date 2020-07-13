85b1fccbe214e365fb8b3e1a60a226381b3825e8
Syed Zaman Mostafiz <56298392+zamanwebdev@users.noreply.github.com>
2020-07-13 21:33:49 +0600

    day31 Completed

---
+++ day31 Completed/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Day 31</title>
+</head>
+<body>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/BQNVP1aEb74" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</body>
+</html>
